c91e6a85fad126321cb9bfea3f8a4d68a03c9224
Pierre-André Mudry <pmudry@gmail.com>
2025-09-07 22:43:57 +0200

    introducing 3d vectors with their maths

--- src/v0_single_threaded/main.cc
+++ src/v0_single_threaded/main.cc
@@ -7,16 +7,18 @@ using namespace std;
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "external/stb_image_write.h"
 
+#include "vec3.h"
+
 // Image dimensions
-const int width = 512;
-const int height = 512;
+const int image_width = 512;
+const int image_height = 512;
 const int channels = 3; // RGB
 
 // Function to write image buffer to PNG file
-void writeImage(const std::vector<unsigned char> &image, int width, int height, const std::string &filename)
+void writeImage(const std::vector<unsigned char> &image, int image_width, int image_height, const std::string &filename)
 {
     const int channels = 3; // RGB
-    if (stbi_write_png(filename.c_str(), width, height, channels, image.data(), width * channels))
+    if (stbi_write_png(filename.c_str(), image_width, image_height, channels, image.data(), image_width * channels))
     {
         std::cout << "Image saved successfully to " << filename << std::endl;
     }
@@ -26,29 +28,54 @@ void writeImage(const std::vector<unsigned char> &image, int width, int height, 
     }
 }
 
-void writeGradientImage(vector<unsigned char> &image)
-{
+/**
+ * Just for the sake of putting a gradient in a file
+ */
+void fillGradientImage(vector<unsigned char> &image){
     // Generate a simple gradient
-    for (int y = 0; y < height; ++y)
+    for (int y = 0; y < image_height; ++y)
     {
-        for (int x = 0; x < width; ++x)
+        for (int x = 0; x < image_width; ++x)
         {
-            int index = (y * width + x) * channels;
-            image[index] = static_cast<unsigned char>(255.0 * y / width);      // Red
-            image[index + 1] = static_cast<unsigned char>(255.0 * x / height); // Green
+            int index = (y * image_width + x) * channels;
+            image[index] = static_cast<unsigned char>(255.0 * y / image_width);      // Red
+            image[index + 1] = static_cast<unsigned char>(255.0 * x / image_height); // Green
+            image[index + 2] = 100;                                            // Blue
+        }
+    } 
+}
+
+void image2Disk(vector<unsigned char> &image, string name){
+    // Generate a simple gradient
+    for (int y = 0; y < image_height; ++y)
+    {
+        for (int x = 0; x < image_width; ++x)
+        {
+            int index = (y * image_width + x) * channels;
+            image[index] = static_cast<unsigned char>(255.0 * y / image_width);      // Red
+            image[index + 1] = static_cast<unsigned char>(255.0 * x / image_height); // Green
             image[index + 2] = 100;                                            // Blue
         }
     }
 
     // Write image to file
-    writeImage(image, width, height, "output.png");
-}
+    writeImage(image, image_width, image_height, name);
+}   
 
-int main()
-{ 
-    // Testing image output
-    std::vector<unsigned char> image(width * height * channels);
-    writeGradientImage(image);
+int main(){ 
+    std::vector<unsigned char> image(image_width * image_height * channels);
+    fillGradientImage(image);
+    
+    image2Disk(image, "output.png");
+
+    // Just some vec3 tests 
+    auto v1 = vec3(1,2,3);
+    auto v2 = vec3(2,2,3);
+    auto v3 = v1 + v2;
+    vec3 v4 = cross(v1, v2);
+
+    clog << v3 << endl;
+    clog << v4 << endl;
 
     return 0;
 }
