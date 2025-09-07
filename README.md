7901473233ab07f90fbf8e09ce921c2f8f57ae11
Pierre-André Mudry <pmudry@gmail.com>
2025-09-07 21:45:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# A simple yet pretty cool ray-tracer in CPP for CS302 HPC
+
+Based on https://github.com/RayTracing/raytracing.github.io/tree/release/src/InOneWeekend, an amazing resource ! 
+
+This is a complete hand-made re-implementation of the `InOneWeekend` version, to get started and understand how it works.
+
+This version is single-threaded, with no GPU acceleration.
+
+It uses [single-file public domain (or MIT licensed) libraries for C/C++](https://github.com/nothings/stb/tree/master).
+
+
+## How to compile
+
+## Using CMAKE
+
+```bash
+cmake .
+```
+
+Then `make` as usual.
+
---
+++ src/v0_single_threaded/main.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "external/stb_image_write.h"
+
+// Image dimensions
+const int width = 512;
+const int height = 512;
+const int channels = 3; // RGB
+
+// Function to write image buffer to PNG file
+void writeImage(const std::vector<unsigned char> &image, int width, int height, const std::string &filename)
+{
+    const int channels = 3; // RGB
+    if (stbi_write_png(filename.c_str(), width, height, channels, image.data(), width * channels))
+    {
+        std::cout << "Image saved successfully to " << filename << std::endl;
+    }
+    else
+    {
+        std::cerr << "Failed to save image to " << filename << std::endl;
+    }
+}
+
+void writeGradientImage(vector<unsigned char> &image){
+        // Generate a simple gradient
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            int index = (y * width + x) * channels;
+            image[index] = static_cast<unsigned char>(255.0 * x / width);      // Red
+            image[index + 1] = static_cast<unsigned char>(255.0 * y / height); // Green
+            image[index + 2] = 100;                                            // Blue
+        }
+    }
+
+    // Write image to file
+    writeImage(image, width, height, "output.png");
+}
+
+int main()
+{    
+    // Testing image output
+    std::vector<unsigned char> image(width * height * channels);
+    writeGradientImage(image);
+
+    return 0;
+}
